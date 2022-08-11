02b1fe7b45288324479697b90961c767997f45ea
FedeCraviotto <fcraviotto.dev@gmail.com>
2022-08-11 10:21:37 -0300

    2da Preentrega sin entregar

--- app.js
+++ app.js
@@ -1,273 +1,231 @@
-
-
-let rutina = {
-    nombre: "",
-    sesiones: []
+let rutina;
+if (localStorage.getItem('rutina')){
+    rutina = JSON.parse(localStorage.getItem('rutina'));
+} else {
+    rutina = [];
 }
+let nombreDeSesionIngresado = ""; 
+let numeroDeSesionIngresada = 0;
+let nombreDeEjercicioIngresado = "";
+let numeroDeEjercicioIngresado = 0;
 
-function armarRutina() {
-    
-    let pedirNombreRutina = prompt('Nombre Rutina?')
-    let pedirCantSesiones = parseInt(prompt('CantSesiones'));
-    
-    rutina.nombre = pedirNombreRutina
+// Da contenido al array rutina. Actualiza storage. Renderiza.
+function crearSesiones() {
+
+    let salir = confirm('A continuación vamos a agregar sesiones, desea continuar?')
+    if (!salir) {return}
+
+    let pedirCantSesiones = parseInt(prompt('¿Cuantos días a la semana vas a entrenar?.\nVamos a armar 1 sesión por cada día de entrenamiento. Si no querés que tome mucho tiempo, podés solamente probar con 2 sesiones de 2 ejercicios cada una.\n\nIngresá la cantidad de sesiones para tu rutina.\nTiene que ser un número. Generalmente no son más de 4.\nEn el próximo paso podrás ponerle nombre a cada sesión'));
     
     for(let i=1; i<=pedirCantSesiones; i++) {
         
-        let nombreSesion = prompt('Ingrese nombre de la sesión ' + i)
-        rutina.sesiones.push({
+        let nombreSesion = prompt('Ingresa un nombre para la sesión ' + i + '\nDebe ser un string')
+        rutina.push({
             nombre:nombreSesion,
             ejercicios: []
         })
     };
-    
-    for (let i=0; i<rutina.sesiones.length; i++) {
-        let pedirCantEjercicios = parseInt(prompt('Cantidad de Ejercicios para la sesión ' + (i+1) + ' ?'));
-    for (let j=0; j<pedirCantEjercicios; j++){
-        let nombreEj = prompt('Nombre del Ejercicio Nro ' + (j+1))
-        rutina.sesiones[i].ejercicios.push(
-            {
-                nombre:nombreEj,
-                pesosBase:[],
-                seriesBase:[],
-                seriesRealizadas:[],
-                ultimosPesos:[],
-                proximosPesos: [],
-                proximasSeries:[]
+
+    rutina.forEach((cadaSesion, i) => {
+        let cantidadEjercicios = parseInt(prompt('Ingresa la cantidad de ejercicios que quieres realizar en la sesión ' + (i+1) + '\nDebe ser un número'));
+        for (let i=0; i<cantidadEjercicios; i++){
+            let nombreEjercicio = prompt('Ingresa el nombre del Ejercicio Nro ' + (i+1) + '\nDebe ser un string')
+            cadaSesion.ejercicios.push(
+                {
+                    nombre:nombreEjercicio,
+                    seriesBase:[],
+                    seriesRealizadas:[],
+                    ultimosPesos:[],
+                    proximosPesos: [],
+                    proximasSeries:[]
+                }
+                );
             }
-            );
-        }
-    }
-    
-    for (let i=0; i<rutina.sesiones.length; i++) {
-        for (let j=0; j<rutina.sesiones[i].ejercicios.length; j++){ 
-            let pedirSeries = parseInt(prompt('Cantidad de series para el ejercicio "' + rutina.sesiones[i].ejercicios[j].nombre + '" de la sesión "'+ rutina.sesiones[i].nombre + '"?' ));
-            
-            for(let k=1; k<=pedirSeries; k++) {
-                let pedirRepsPorSerie = prompt('Ingresá el rango de repeticiones para la serie ' + k +  ' del Ejericicio ' + rutina.sesiones[i].ejercicios[j].nombre + '\nDos números separados por un espacio\nRepsMínimas-espacio-RepsMáximas');
-                let arrayRangoReps = pedirRepsPorSerie.split(' ').map((string) => {
-                    return parseInt(string);
-                })
-                rutina.sesiones[i].ejercicios[j].seriesBase.push(arrayRangoReps);
-            }
-            
-            let pedirPesos = prompt('Ingresá los pesos para cada serie del ejercicio ' + rutina.sesiones[i].ejercicios[j].nombre + ', separados por espacios\nEjemplo 45 50 45 (peso-espacio-peso-espacio-peso)');
-            let arrayDePesos = pedirPesos.split(' ').map((string) => {
+    })
+
+salir = confirm('Desea continuar?')
+if (!salir) {return}
+
+rutina.forEach((cadaSesion) => {
+
+    cadaSesion.ejercicios.forEach((ejercicio) => {
+
+        let cantidadSeries = parseInt(prompt('¿Cuantas series de  "' + ejercicio.nombre + '" vas a querer hacer por sesión?.\nIngresá un número'));
+        for(let i=1; i<=cantidadSeries; i++) {
+            let rangoRepeticionesPorSerie = prompt('Ingresá los rangos para cada serie.\n\nVoy a pedirte que ingreses dos numeros:\nPrimero el mínimo de repeticiones, luego un espacio, y luego el máximo de repeticiones que vas a queres hacer para la serie ' + i +  ' del Ejericicio ' + ejercicio.nombre + '\n\nPor ejemplo:\nSi quisieras realizar mínimo 8 y máximo 15 para esa serie, deberías ingresar "8 15", es decir "ocho espacio quince"\nRepsMínimas-espacio-RepsMáximas');
+            let arrayRangoReps = rangoRepeticionesPorSerie.split(' ').map((string) => {
                 return parseInt(string);
             })
-            rutina.sesiones[i].ejercicios[j].pesosBase = arrayDePesos;
-                    
+            ejercicio.seriesBase.push(arrayRangoReps);
         }
-    }
-    
+        let pedirPesos = prompt('Ingresá de una sola vez, todos los pesos iniciales para cada serie del ejercicio ' + ejercicio.nombre + '.\n\nPor ejemplo, si voy a hacer 2 series para mi ejercicio, y quisiera levantar 50 kilos en la primera serie y 60 kilos en la segunda, debería ingresarlo de la siguiente manera: "50 60".\nEs decir peso-espacio-peso\n\nTe recuerdo que al ejercicio ' + ejercicio.nombre + ' le asignaste una cantidad de ' + ejercicio.seriesBase.length  + ' series.');
+        let arrayDePesos = pedirPesos.split(' ').map((string) => {
+            return parseInt(string);
+        })
+        ejercicio.proximosPesos = arrayDePesos;
+    })
+})
+localStorage.setItem('rutina', JSON.stringify(rutina))
+crearTablas()
+alert('Rutina creada exitosamente!')
 }
-
-let nombreDeSesionDado = "";
-let numeroDeSesion = 0;
 
 function pedirSesion () {
-    nombreDeSesionDado = prompt('Ingrese nombre de la sesión a modificar');
+    let userInput = prompt('Ingrese nombre de la sesión a modificar, agregar, o eliminar\nDebe ser un string.')
+    nombreDeSesionIngresado = userInput;
 }
 
-
 function obtenerIndice() {  
-    for (let i=0; i<rutina.sesiones.length; i++) {
-        if (rutina.sesiones[i].nombre==nombreDeSesionDado) {
-            numeroDeSesion= i;
+    rutina.forEach((sesion, i) => {
+        if (sesion.nombre==nombreDeSesionIngresado) {
+            numeroDeSesionIngresada = i;
         }
-    }
+    })
+}
+
+function pedirEjercicio () {
+    let userInput = prompt('Ingrese nombre del ejercicio a modificar, agregar, o eliminar\nDebe ser un string.');
+    if (userInput != '' && userInput != null) {
+        nombreDeEjercicioIngresado = userInput;
+       };
+}
+
+function obtenerIndiceEjercicio() {  
+    rutina.forEach((sesion) => {
+        sesion.ejercicios.forEach((ejercicio, i) => {
+            if (ejercicio.nombre==nombreDeEjercicioIngresado) {
+                numeroDeEjercicioIngresado = i;
+            }
+
+        })
+    })
 }
 
 function anotarReps (num) {
-    for (let i=0; i<rutina.sesiones[num].ejercicios.length; i++){ 
-        let seriesPedidas = []
-            for (let j=0; j<rutina.sesiones[num].ejercicios[i].seriesBase.length; j++) {
+    rutina[num].ejercicios.forEach((ejercicio) => {
+        let repeticionesHechas = []
+        ejercicio.seriesBase.forEach((serie, i) => {
+            repeticionesHechas.push(parseInt(prompt('Ingresá la cantidad de repeticiones que hiciste en la serie número ' + (i+1) +  ' del ejercicio ' + ejercicio.nombre + '.\nDebes ingresar un solo número')))
+        })
+        ejercicio.seriesRealizadas = repeticionesHechas;
+    })
+localStorage.setItem('rutina', JSON.stringify(rutina))
+}
 
-            seriesPedidas.push(parseInt(prompt('Ingresá la cantidad de repeticiones hechas para la serie número ' + (j+1) +  ' del ejercicio ' + rutina.sesiones[num].ejercicios[i].nombre + '.\nRecordá ingresar solo un número -por cada serie-')))
+// Modifica el array rutina. Actualiza storage. Borra y renderiza.
+    function anotarRepeticiones(){
+        pedirSesion ()
+        obtenerIndice()
+        anotarReps(numeroDeSesionIngresada) 
+        localStorage.setItem('rutina', JSON.stringify(rutina))
+        reemplazarSesiones()
         }
-        rutina.sesiones[num].ejercicios[i].seriesRealizadas = seriesPedidas;
-    }
-}
 
-function ultimosBase(num) {
-    for (let i=0; i<rutina.sesiones[num].ejercicios.length; i++) {
-        rutina.sesiones[num].ejercicios[i].ultimosPesos = rutina.sesiones[num].ejercicios[i].pesosBase     
-    }
-}
-
-function ultimosProximos(num) {
-    for (let i=0; i<rutina.sesiones[num].ejercicios.length; i++) {
-            rutina.sesiones[num].ejercicios[i].ultimosPesos = rutina.sesiones[num].ejercicios[i].proximosPesos
-        
-    }
+    function equipararUltimosPesosConProximos(num) {
+        rutina[num].ejercicios.forEach((ejercicio) => {
+            ejercicio.ultimosPesos = ejercicio.proximosPesos;
+    })
 }
 
 function actualizarPesos(num) {
-    for (let i=0; i<rutina.sesiones[num].ejercicios.length; i++) {
+    rutina[num].ejercicios.forEach((ejercicio) => {
         let nuevosPesos = [];
-        
-        for (let j=0; j<rutina.sesiones[num].ejercicios[i].seriesRealizadas.length; j++) {
-            
-            if (rutina.sesiones[num].ejercicios[i].seriesRealizadas[j]>=rutina.sesiones[num].ejercicios[i].seriesBase[j][1]) {
-                nuevosPesos.push(rutina.sesiones[num].ejercicios[i].ultimosPesos[j] + 5)
+        ejercicio.seriesRealizadas.forEach((serie, i) =>{
+            if (serie>=ejercicio.seriesBase[i][1]) {
+                nuevosPesos.push(ejercicio.ultimosPesos[i] + 5)
             } else {
-                nuevosPesos.push(rutina.sesiones[num].ejercicios[i].ultimosPesos[j])
+                nuevosPesos.push(ejercicio.ultimosPesos[i])
             }
-        }
-        rutina.sesiones[num].ejercicios[i].proximosPesos = nuevosPesos;
-    }  
-};
+        })
+        ejercicio.proximosPesos = nuevosPesos;
+    })
+}
 
 function actualizarSeries(num) {
-    for (let i=0; i<rutina.sesiones[num].ejercicios.length; i++) {
-        let nuevasRepeticiones = [];
-        for (let j=0; j<rutina.sesiones[num].ejercicios[i].seriesRealizadas.length; j++) {
-            if (rutina.sesiones[num].ejercicios[i].seriesRealizadas[j]>=rutina.sesiones[num].ejercicios[i].seriesBase[j][1]){
-                nuevasRepeticiones.push(rutina.sesiones[num].ejercicios[i].seriesBase[j]);
+    rutina[num].ejercicios.forEach((ejercicio) => {
+        let nuevasSeries = [];
+        ejercicio.seriesRealizadas.forEach((serie, i) => {
+            if (serie>=ejercicio.seriesBase[i][1]) {
+                nuevasSeries.push(ejercicio.seriesBase[i]);
             } else {
-                let rango = [];
-                rango.push(rutina.sesiones[num].ejercicios[i].seriesRealizadas[j])
-                rango.push(rutina.sesiones[num].ejercicios[i].seriesBase[j][1]);
-                nuevasRepeticiones.push(rango)
+                let nuevoRangoDeSeries = [];
+                nuevoRangoDeSeries.push(serie);
+                nuevoRangoDeSeries.push(ejercicio.seriesBase[i][1]);
+                nuevasSeries.push(nuevoRangoDeSeries);
             }
-            
-        }
-        rutina.sesiones[num].ejercicios[i].proximasSeries = nuevasRepeticiones;
-    }
+        })
+        ejercicio.proximasSeries = nuevasSeries;
+    })
 }
 
-function mostrarProxima () {
-    let proximaSesion = [];
-    for (let i=0; i<rutina.sesiones[numeroDeSesion].ejercicios.length; i++){
-        let {nombre, proximosPesos, proximasSeries} = rutina.sesiones[numeroDeSesion].ejercicios[i];
-        let objEjercicio = {
-            Ejercicio:nombre,
-            proximosPesos: proximosPesos,
-            proximasSeries: proximasSeries
+// Modifica el array rutina. Actualiza storage. Borra y renderiza.
+function generarProximaSesion () {
+    pedirSesion ()
+    obtenerIndice()
+    equipararUltimosPesosConProximos(numeroDeSesionIngresada)
+    actualizarPesos(numeroDeSesionIngresada)
+    actualizarSeries(numeroDeSesionIngresada)
+    localStorage.setItem('rutina', JSON.stringify(rutina))
+    reemplazarSesiones()
+}
+
+
+
+
+
+
+// function mostrarProximaSesion (num) {
+//     let proximaSesion = [];
+//     rutina[num].ejercicios.forEach((ejercicio) => {
+//         let {nombre, proximosPesos, proximasSeries} = ejercicio;
+//         let datosDeCadaEjercicio = {
+//             Ejercicio:nombre,
+//             proximosPesos: proximosPesos,
+//             proximasSeries: proximasSeries
         
-        }
-        proximaSesion.push(objEjercicio)
-    }
-    console.log(proximaSesion);
-}
-
-function anotarSesion(){
-    pedirSesion ()
-    obtenerIndice()
-    anotarReps(numeroDeSesion) 
-    }
-
-function proximaSesionUnica () {
-    pedirSesion ()
-    obtenerIndice()
-    ultimosBase(numeroDeSesion)
-    actualizarPesos(numeroDeSesion)
-    actualizarSeries(numeroDeSesion)
-    mostrarProxima()
-}
-
-function proximaSesion () {
-    pedirSesion ()
-    obtenerIndice()
-    ultimosProximos(numeroDeSesion)
-    actualizarPesos(numeroDeSesion)
-    actualizarSeries(numeroDeSesion)
-    mostrarProxima()
-}
-
-
-// let rutina = {
-//     nombre: "Mesociclo01",
-//     sesiones: [
-//         {
-//             nombre:"Lunes",
-//             ejercicios: [
-//             {
-//                 nombre:"Jalon",
-//                 seriesBase: [[9,12], [9,12], [9,12]],
-//                 pesosBase:[30, 35, 35],
-//                 seriesRealizadas:[10, 15, 15],
-//                 ultimosPesos: [30, 35, 35],
-//                 proximosPesos: [35, 40, 35],
-//                 proximasSeries:[[9, 12], [9, 12], [8, 12]]
-//             },
-//             {
-//                 nombre:"Remo",
-//                 seriesBase: [[6, 9], [6, 9]],
-//                 pesosBase:[55, 60],
-//                 seriesRealizadas:[10, 8],
-//                 ultimosPesos: [55, 60],
-//                 proximosPesos: [55, 60],
-//                 proximasSeries:[[5, 9],[6, 9]]
-//             }]
-//         },
-//          {   
-//             nombre:"Martes",
-//             ejercicios:[
-
-//             {
-//                 nombre:"Curl",
-//                 seriesBase: [[9,12], [9,12], [9,12]],
-//                 pesosBase:[30, 35, 35],
-//                 seriesRealizadas:[13, 13, 11],
-//                 ultimosPesos: [30, 35, 35],
-//                 proximosPesos: [35, 40, 35],
-//                 proximasSeries:[[9,12], [9, 12], [10, 12]]
-//             },
-//             {
-//                 nombre:"Sentadillas",
-//                 seriesBase: [[6, 9], [6, 9]],
-//                 pesosBase:[55, 60],
-//                 seriesRealizadas:[20, 3],
-//                 ultimosPesos: [55, 60],
-//                 proximosPesos: [60, 60],
-//                 proximasSeries:[[6, 9], [3, 9]]
-//             }
-//         ]
-//     }
-// ]
+//         }
+//         proximaSesion.push(datosDeCadaEjercicio)
+//     })
+//     console.log(proximaSesion);
 // }
 
 
 
-//EVENTOS
 
-const btn_armarRutina = document.querySelector('#btn_armarRutina');
-
-btn_armarRutina.addEventListener('click', armarRutina);
-
-const btn_anotarReps = document.querySelector('#btn_anotarReps');
-
-btn_anotarReps.addEventListener('click', anotarSesion);
-
-const btn_proximaSesion = document.querySelector('#btn_proximaSesion');
-
-btn_proximaSesion.addEventListener('click', proximaSesion);
-
-const btn_proximaSesionUnica = document.querySelector('#btn_proximaSesionUnica');
-
-btn_proximaSesionUnica.addEventListener('click', proximaSesionUnica);
-
-
-
-/*
-Display :
-
-Todas las tablas solamente van a tener esas 4 columnas.
-Sino, que las tablas de sesiones actuales tengan todo, y que las de proxima semana tengan solo las 4 columnas.
-
-Subtitulo "Sesiones semana actual"
-1 Tabla por sesión (Una para Lunes, otra para Martes, etc) va a tener los <th>
-
-Y mediante DOM vamos a traer a la tabla (contenedor), y vamos a crear elementos <tr> con sus <td> y les vamos a hacer append.
-Hasta ahí la tabla creada, vacía.
-
-Después, vamos a poder traer cada <td> y con innerText, y la ayuda del forEach, vamos a poder llenarlas con los datos contenidos en cada objeto ejercicio.
-Esto de a una tabla, interactuar con el numeroDeSesion. De la misma manera que pedimos y modificamos de a 1 sesión, el DOM que trabaje igual.
-
-Subtitulo "Sesiones proxima semana"
-Vamos a tener que replicar tantas tablas para esta sección, como sesiones haya.
-
-Por último, modificar mostrarProxima() para que en vez de hacer un console.log del objeto, lo retorne, para almacenarlo en algún lugar y pasar los datos a la tabla a través del DOM
-
-*/
+/*Objeto final, luego de actualizados los pesos y repeticiones, y hasta antes de mostrarSesion()*/
+// let rutina = [
+//     {
+//         nombre:"Lunes",
+//         ejercicios: [
+//               {
+//                 nombre: "Jalon",
+//                 seriesBase: [[8, 12],[8, 12],[10, 20]],
+//                 seriesRealizadas: [12, 15, 20],
+//                 ultimosPesos: [30, 35, 40],
+//                 proximosPesos: [35, 40, 45],
+//                 proximasSeries: [[8, 12],[8, 12],[10, 20]],
+//               },
+//               {
+//                 nombre: "Remo",
+//                 seriesBase: [[6, 9], [6, 9]],
+//                 seriesRealizadas: [13, 5],
+//                 ultimosPesos: [50, 55],
+//                 proximosPesos: [55, 55],
+//                 proximasSeries: [[6, 9], [5, 9]],
+//               }
+//         ]
+//     },
+//     {
+//         nombre:"Martes",
+//         ejercicios: [
+//             {
+//                 nombre: "Sentadilla",
+//                 seriesBase: [[7, 5], [7, 5], [8, 12]],
+//                 seriesRealizadas: [],
+//                 ultimosPesos: [],
+//                 proximosPesos: [50, 60, 60],
+//                 proximasSeries: [],
+//               }
+//         ]
+//     }
+// ];
